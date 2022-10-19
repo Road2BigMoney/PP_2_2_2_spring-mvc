1d0d66af1a8d9f70d875a3fc680cf35f5e14f346
Road2BigMoney <usaakk99@gmail.com>
2022-10-20 02:22:13 +0300

    change componentScan

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,6 +1,8 @@
 package services;
 
 import model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
